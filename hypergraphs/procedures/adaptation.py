9328a2a07e62ab84d123e1964ae995dda629f650
Filip Galas <fgalas525@gmail.com>
2019-01-16 16:38:02 +0100

    Add P3 adaptation

--- hypergraphs/procedures/adaptation.py
+++ hypergraphs/procedures/adaptation.py
@@ -1,7 +1,7 @@
 import networkx as nx
 from PIL import Image
 
-from productions import Direction, P2
+from productions import Direction, P2, P3
 from utils import get_node_id
 
 
@@ -43,16 +43,24 @@ def mark_hyperedges_for_adaptation(graph: nx.Graph, epsilon: int, image: Image):
 
     # Iterate B edges
     for edge_id, edge_data in B_hyperedges:
-        # TODO We have to extract B node, two I nodes and one V node for given B node id, please refer to task docs,
-        # TODO just take a look at the picture and you will know what to use in P3 production. The question is how
-        # TODO to determine the proper nodes to pass in P3. There is possible error in P2.
         # B should have two neighbors
-        b_neighbors = [graph.node[v] for v in graph.neighbors(edge_id)]
-        v_neighbor_position = (b_neighbors[0]['x'] + b_neighbors[1]['x']) // 2, (
-                b_neighbors[0]['y'] + b_neighbors[1]['y']) // 2
-        v_id = get_node_id(v_neighbor_position)
-        # P3(graph, ,edge_id, ,image)
-        pass
+        v1, v2 = graph.neighbors(edge_id)
+        I_neigh1 = [e for e in graph.neighbors(v1) if graph.nodes[e]['label'] == 'I']
+        I_neigh2 = [e for e in graph.neighbors(v2) if graph.nodes[e]['label'] == 'I']
+        I1 = None
+        I2 = None
+        F = None
+        for i1 in I_neigh1:
+            for i2 in I_neigh2:
+                common_neighbors = [v for v in graph.neighbors(i1) if v in graph.neighbors(i2)]
+                if len(common_neighbors) == 1:
+                    v = common_neighbors[0]
+                    I1 = i1
+                    I2 = i2
+                    F = next(e for e in graph.neighbors(v) if graph.nodes[e]['label'] in Direction)
+                    break
+
+        P3(graph, edge_id, [I1, I2], F, image)
 
     # Iterate F edges
     for edge_id, edge_data in F_hyperedges:
