e0ec5c01e7f03838d3f508ec15a6fe458334244f
Maciej Rapacz <maciek.rapacz@gmail.com>
2019-01-17 14:09:26 +0100

    Fix adaptation

--- hypergraphs/procedures/adaptation.py
+++ hypergraphs/procedures/adaptation.py
@@ -1,8 +1,9 @@
 import networkx as nx
 from PIL import Image
 
+from plot import plot
 from procedures.approx_error import approx_error
-from productions import P2, P3, P5, P6, P4
+from productions import P2, P3, P5, P6, P4, P3AutoDetect
 from utils import get_node_id, Direction
 
 
@@ -12,42 +13,29 @@ def calculate_aproximation_error():
 
 
 def mark_hyperedges_for_adaptation(graph: nx.Graph, epsilon: int, image: Image):
-    hyperedges = [(id, node_data) for id, node_data in graph.nodes(data=True) if node_data['is_hyperedge']]
-    I_hyperedges = [(id, node_data) for id, node_data in hyperedges if node_data['label'] == 'I']
-    B_hyperedges = [(id, node_data) for id, node_data in hyperedges if node_data['label'] == 'B']
-    F_hyperedges = [(id, node_data) for id, node_data in hyperedges if node_data['label'] in Direction]
+    for _ in range(3):
+        hyperedges = [(id, node_data) for id, node_data in graph.nodes(data=True) if node_data['is_hyperedge']]
+        I_hyperedges = [(id, node_data) for id, node_data in hyperedges if node_data['label'] == 'I']
+        B_hyperedges = [(id, node_data) for id, node_data in hyperedges if node_data['label'] == 'B']
+        F_hyperedges = [(id, node_data) for id, node_data in hyperedges if node_data['label'] in Direction]
 
-    # Iterate I edges
-    for edge_id, edge_data in I_hyperedges:
-        if approx_error(image, graph, edge_id) > epsilon:
+        # Iterate I edges
+        for edge_id, edge_data in I_hyperedges:
+            # if approx_error(image, graph, edge_id) > epsilon:
             P5(graph, edge_id, image)
-            P6(graph, edge_id, image)
+            # P6(graph, edge_id, image)
 
-    # Iterate I edges
-    for edge_id, edge_data in I_hyperedges:
-        P2(graph, edge_id, image)
+        # Iterate I edges
+        for edge_id, edge_data in I_hyperedges:
+            P2(graph, edge_id, image)
 
-    # Iterate B edges
-    for edge_id, edge_data in B_hyperedges:
-        # B should have two neighbors
-        v1, v2 = graph.neighbors(edge_id)
-        I_neigh1 = [e for e in graph.neighbors(v1) if graph.nodes[e]['label'] == 'I']
-        I_neigh2 = [e for e in graph.neighbors(v2) if graph.nodes[e]['label'] == 'I']
-        I1 = None
-        I2 = None
-        F = None
-        for i1 in I_neigh1:
-            for i2 in I_neigh2:
-                common_neighbors = [v for v in graph.neighbors(i1) if v in graph.neighbors(i2)]
-                if len(common_neighbors) == 1:
-                    v = common_neighbors[0]
-                    I1 = i1
-                    I2 = i2
-                    F = next(e for e in graph.neighbors(v) if graph.nodes[e]['label'] in Direction)
-                    break
+        # Iterate B edges
+        for edge_id, edge_data in B_hyperedges:
+            # B should have two neighbors
+            P3AutoDetect(graph, edge_id, image)
 
-        P3(graph, edge_id, [I1, I2], F, image)
+        # Iterate F edges
+        for edge_id, edge_data in F_hyperedges:
+            P4(graph, edge_id, image)
 
-    # Iterate F edges
-    for edge_id, edge_data in F_hyperedges:
-        P4(graph, edge_id, image)
+        plot(graph)
--- hypergraphs/tests/test_adaptation.py
+++ hypergraphs/tests/test_adaptation.py
@@ -16,11 +16,12 @@ class TestAdaptation(TestCase):
     def setUp(self):
         self.graph = nx.Graph()
         self.image = Image.open(IMAGE_PATH)
-        self.hyperedge = self.prepare_graph_and_get_central_hyperedge()
-        self.added_node_position = (self.hyperedge[1]['x'], self.hyperedge[1]['y'])
-        self.added_node_id = get_node_id(self.added_node_position)
+        width, height = self.image.size
+        P1(self.graph, x_max_idx=width - 1, y_max_idx=height - 1, image=self.image)
+        # self.added_node_position = (self.hyperedge[1]['x'], self.hyperedge[1]['y'])
+        # self.added_node_id = get_node_id(self.added_node_position)
         # plot(self.graph)
-        P2(self.graph, hyperedge_id=self.hyperedge[0], image=self.image)
+        # P2(self.graph, hyperedge_id=self.hyperedge[0], image=self.image)
         # self.sorted_nodes_with_data = sorted(self.graph.nodes(data=True), key=lambda x: (x[1]['x'], x[1]['y']))
         # plot(self.graph)
 
@@ -30,10 +31,10 @@ class TestAdaptation(TestCase):
         # hyperedges_to_remove = [x for x, y in self.graph.nodes(data=True) if 'label' in y.keys() and y['label'] == 'B']
         # for id in hyperedges_to_remove:
         #     self.graph.remove_node(id)
-        hyperedge = [(x, y) for x, y in self.graph.nodes(data=True) if 'label' in y.keys() and y['label'] == 'I'][0]
-        hyperedge[1]['should_break'] = 1
-        self.graph.add_node(hyperedge[0], **hyperedge[1])
-        return hyperedge
+        # hyperedge = [(x, y) for x, y in self.graph.nodes(data=True) if 'label' in y.keys() and y['label'] == 'I'][0]
+        # hyperedge[1]['should_break'] = 1
+        # self.graph.add_node(hyperedge[0], **hyperedge[1])
+        # return hyperedge
 
     def testAdaptation(self):
         mark_hyperedges_for_adaptation(self.graph, 0.1, self.image)
