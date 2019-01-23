af950a8b21c36ad1b44a430a5660969143c2e23b
Patryk Duda <patryk.d@10g.pl>
2019-01-23 23:14:06 +0100

    Don't run P5 if should_break == 1

--- hypergraphs/productions/p5.py
+++ hypergraphs/productions/p5.py
@@ -7,8 +7,9 @@ from hypergraphs.utils import HyperEdge
 
 def P5(graph: nx.Graph, hyperedge_id, image: Image):
     __check_conditions(graph, hyperedge_id)
-    graph.node[hyperedge_id]['should_break'] = 1
-    P6(graph, hyperedge_id, image)
+    if graph.node[hyperedge_id]['should_break'] is 0:
+        graph.node[hyperedge_id]['should_break'] = 1
+        P6(graph, hyperedge_id, image)
 
 
 def __check_conditions(graph, hyperedge_id):
@@ -16,5 +17,3 @@ def __check_conditions(graph, hyperedge_id):
         raise ValueError('Given node_id is not id of hyperedge')
     if graph.node[hyperedge_id]['label'] is not HyperEdge.I.name:
         raise ValueError('Given hyperedge label is not I')
-    elif graph.node[hyperedge_id]['should_break'] is not 0:
-        raise ValueError('Hyper edge has should_break set to 1, while it is supposed to be equal 0')
